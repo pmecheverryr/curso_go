0d85e25a87b45f6b868721615bf56bea77b73d79
Pedro Mauricio Echeverry Rey <pedromauricioecheverry@gmail.com>
2018-11-14 21:04:22 -0500

    Actualizacion tipos de datos

--- 03_tipos-datos/tipos-datos.go
+++ 03_tipos-datos/tipos-datos.go
@@ -21,9 +21,20 @@ func main() {
 	var enteroByte byte // alias para uint8
 	var enteroRune rune // alias para int32
 
+	fmt.Println("// Enteros con signo (-+)")
 	fmt.Println(entero8)
 	fmt.Println(entero16)
 	fmt.Println(entero32)
 	fmt.Println(entero64)
+
+	fmt.Println("// Enteros sin signo (-+)")
+	fmt.Println(uentero8)
+	fmt.Println(uentero16)
+	fmt.Println(uentero32)
+	fmt.Println(uentero64)
+
+	fmt.Println("// Alias")
+	fmt.Println(enteroByte)
+	fmt.Println(enteroRune)
 
 }
